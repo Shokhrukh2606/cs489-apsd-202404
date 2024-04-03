22dd4d089f3a14642154f51b842db0682a827a2a
Shokhrukh1998 <33511637+Shokhrukh2606@users.noreply.github.com>
2024-04-03 08:04:00 -0500

    Lab1b finished

--- lab1b/employeepensionscliapp/src/main/java/edu/miu/cs489/lab1b/model/Employee.java
+++ lab1b/employeepensionscliapp/src/main/java/edu/miu/cs489/lab1b/model/Employee.java
@@ -1,4 +1,76 @@
 package edu.miu.cs489.lab1b.model;
 
+import java.time.LocalDate;
+
 public class Employee {
+    private Long employeeId;
+    private String firstName;
+    private String lastName;
+    private LocalDate employementDate;
+    private double yearlySalary;
+
+    private PensionPlan pensionPlan;
+    public Employee(Long employeeId, String firstName, String lastName, LocalDate employementDate, double yearlySalary, PensionPlan pensionPlan) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.employementDate = employementDate;
+        this.yearlySalary = yearlySalary;
+        this.pensionPlan=pensionPlan;
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getEmployementDate() {
+        return employementDate;
+    }
+
+    public void setEmployementDate(LocalDate employementDate) {
+        this.employementDate = employementDate;
+    }
+
+    public double getYearlySalary() {
+        return yearlySalary;
+    }
+
+    public void setYearlySalary(double yearlySalary) {
+        this.yearlySalary = yearlySalary;
+    }
+
+    public PensionPlan getPensionPlan() {
+        return pensionPlan;
+    }
+
+    public void setPensionPlan(PensionPlan pensionPlan) {
+        this.pensionPlan = pensionPlan;
+    }
+
+    public String toJSONString() {
+        return pensionPlan==null?String.format("\t{ \"employeeId\":%d, \"firstName\":\"%s\", \"lastName\":\"%s\", \"employementDate\":\"%s\", \"yearlySalary\":%.2f, \"pensionPlan\":null }",
+                employeeId, firstName, lastName, employementDate, yearlySalary):String.format("\t{ \"employeeId\":%d, \"firstName\":\"%s\", \"lastName\":\"%s\", \"employementDate\":\"%s\", \"yearlySalary\":%.2f, \"pensionPlan\": %s } }",
+                employeeId, firstName, lastName, employementDate, yearlySalary, pensionPlan.toJSONString());
+    }
+
 }
--- lab1b/employeepensionscliapp/src/main/java/edu/miu/cs489/lab1b/model/PensionPlan.java
+++ lab1b/employeepensionscliapp/src/main/java/edu/miu/cs489/lab1b/model/PensionPlan.java
@@ -1,4 +1,44 @@
 package edu.miu.cs489.lab1b.model;
 
+import java.time.LocalDate;
+
 public class PensionPlan {
+    private String planReferenceNumber;
+    private LocalDate enrollmentDate;
+    private Double monthlyContribution;
+
+    public PensionPlan(String planReferenceNumber, LocalDate enrollmentDate, Double monthlyContribution) {
+        this.planReferenceNumber = planReferenceNumber;
+        this.enrollmentDate = enrollmentDate;
+        this.monthlyContribution = monthlyContribution;
+    }
+
+    public String getPlanReferenceNumber() {
+        return planReferenceNumber;
+    }
+
+    public void setPlanReferenceNumber(String planReferenceNumber) {
+        this.planReferenceNumber = planReferenceNumber;
+    }
+
+    public LocalDate getEnrollmentDate() {
+        return enrollmentDate;
+    }
+
+    public void setEnrollmentDate(LocalDate enrollmentDate) {
+        this.enrollmentDate = enrollmentDate;
+    }
+
+    public Double getMonthlyContribution() {
+        return monthlyContribution;
+    }
+
+    public void setMonthlyContribution(Double monthlyContribution) {
+        this.monthlyContribution = monthlyContribution;
+    }
+
+    public Object toJSONString() {
+        return String.format("\t{ \"planReferenceNumber\":%s, \"enrollmentDate\":\"%s\", \"monthlyContribution\":\"%,.2f\" }",
+                planReferenceNumber,enrollmentDate,monthlyContribution);
+    }
 }
